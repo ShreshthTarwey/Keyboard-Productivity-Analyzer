1cc54a3bfaad2b2e95a2428aa143faab56c4670a
Shreshth Tarwey <shreshthtarwey3010@gmail.com>
2025-12-18 20:18:42 +0530

    Added a simple GUI for users

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-from key_logger import KeyLogger
-import sys
+import tkinter as tk
+from gui import TrackerGUI
 
 def main():
-    print("Welcome to Productivity Analyzer")
-    logger = KeyLogger()
-    
-    try:
-        print("Recording... Press Ctrl+C to stop in terminal if needed.")
-        logger.start()
-    except KeyboardInterrupt:
-        print("\nStopping...")
-
-    # Always generate report (whether stopped by ESC or Ctrl+C)
-    log_data = logger.get_log()
-    print(f"Captured {len(log_data)} keystrokes.")
-    
-    # Analysis Phase
-    from analyzer import Analyzer
-    print("Analyzing session...")
-    analyzer = Analyzer(log_data)
-    print(analyzer.analyze())
+    root = tk.Tk()
+    app = TrackerGUI(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
